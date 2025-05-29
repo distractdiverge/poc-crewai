2f702db8ee3a6c8742a115aae77d694c3244cbdb
Allie Lapinski <alex.lapinski@gmail.com>
2025-05-29 10:15:14 -0400

    prep for chainlit

--- src/poc_crewai_developers/main.py
+++ src/poc_crewai_developers/main.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 import sys
-import warnings
-
 from datetime import datetime
-
 from poc_crewai_developers.crew import PocCrewaiDevelopers
-
-warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
 # This main file is intended to be a way for you to run your
 # crew locally, so refrain from adding unnecessary logic into this file.
